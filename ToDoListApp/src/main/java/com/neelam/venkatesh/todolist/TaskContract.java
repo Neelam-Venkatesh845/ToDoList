5be93a1121916ceaf2374264f3bb3e46ee9d792c
Neelam-Venaktesh <n.venkateshvvv@gmail.com>
2016-11-07 22:11:57 +0530

    Todolist app
    Todolist app for first time

---
+++ ToDoListApp/src/main/java/com/neelam/venkatesh/todolist/TaskContract.java
@@ -0,0 +1,40 @@
+package com.neelam.venkatesh.todolist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.neelam.venaktesh.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+
+}
+
+    class TaskDbHelper extends SQLiteOpenHelper {
+
+    public TaskDbHelper(Context context) {
+        super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TaskContract.TaskEntry.TABLE + " ( " +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE);
+        onCreate(db);
+    }
+}
